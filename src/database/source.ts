dd1b0bd91bf74d2c2662a39a41c19f7b9564b458
ïago alexandre <iagoaap16@gmail.com>
2024-12-04 20:53:43 -0300

    FEAT: finish typeorm config

--- src/database/source.ts
+++ src/database/source.ts
@@ -5,14 +5,13 @@ Environment.assertInitialized();
 
 export const datasource = new DataSource({
   type: 'mysql',
-	timezone: 'Z',
+	timezone: '-00:00',
   port: Environment.vars.MYSQL_PORT,
   host: Environment.vars.MYSQL_HOST,
   username: Environment.vars.MYSQL_USER,
 	password: Environment.vars.MYSQL_PASS,
 	database: Environment.vars.MYSQL_DATABASE,
 	logging: Environment.vars.DB_LOGGING,
-  ssl: {},
 	entities: [
 		Environment.getType() === 'prod' ? '**/entities/*.js' : '**/entities/*.ts',
 	],
